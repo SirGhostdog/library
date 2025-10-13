a1e71666b2aea90fdfafcaaafbe637f98e95c06f
Alex Hallett <ajhallett98@gmail.com>
2025-10-12 23:58:07 -0500

    Add sudo code for script

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const myLibrary = [];
 
-function Book(title, author, pages) {
+function Book(title, author, pages, read) {
     this.title = title;
-    this.author = author;
-    this.pages = pages;
+    this.author = "by" + author;
+    this.pages = "p." + pages;
+    this.read = read
+    this.BookInfo = function() {
+        return (this.title, this.author, this.pages, this.read)
+    }
 }
 
 function addBookToLibrary() {
-
+ON BUTTON CLICK 
+ADD NEW BOOK TO LIBRARY
+RETURN BOOK INFO OF NEW BOOK FROM LIBRARY
 }
+
+TAKE myLibrary ARRAY AND ADD HTML DOM FOR EACH OBJECT
