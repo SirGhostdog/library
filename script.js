e91892121108af73ff5ee35f918f0d82c5a37aeb
Alex Hallett <ajhallett98@gmail.com>
2025-10-14 23:11:58 -0500

    Add function to open and close prompt window and save data

--- script.js
+++ script.js
@@ -1,23 +1,55 @@
 const myLibrary = [];
 
+const addPrompt = document.getElementById('add-book-prompt');
+
+document.addEventListener('submit', function(event){
+    
+    event.preventDefault()
+
+    const promptData = new FormData(addPrompt);
+
+    const title = promptData.get('title')
+    const author = promptData.get('author')
+    const pages = promptData.get('pages')
+    const read = promptData.get('read')
+
+    addPrompt.style.display = "none";
+    addPrompt.reset();
+    }
+);
+
+document.getElementById('cancel-button').addEventListener("click", function(){
+    addPrompt.style.display = "none";
+    addPrompt.reset();
+        }
+);
+
+document.getElementById('add-book').addEventListener("click", function(){
+    addPrompt.style.display = "grid";
+        }
+);
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = "by" + author;
     this.pages = "p." + pages;
     this.read = read
     this.BookInfo = function() {
-        return (this.title, this.author, this.pages, this.read)
+        console.log (this.title, this.author, this.pages, this.read)
     }
-}
+};
 
-const myButton = document.getElementById('add-book')
-const addBookPrompt = document.getElementById('add-book-prompt')
-const submitButton = document.getElementById('submit-button')
-const cancelButton = document.getElementById('cancel-button')
-function addBookToLibrary() {
-ON BUTTON CLICK 
-ADD NEW BOOK TO LIBRARY
-RETURN BOOK INFO OF NEW BOOK FROM LIBRARY
-}
 
-TAKE myLibrary ARRAY AND ADD HTML DOM FOR EACH OBJECT
+
+
+
+
+
+// function addBookToLibrary() {
+    
+// ON BUTTON CLICK 
+// ADD NEW BOOK TO LIBRARY
+// RETURN BOOK INFO OF NEW BOOK FROM LIBRARY
+// }
+
+// TAKE myLibrary ARRAY AND ADD HTML DOM FOR EACH OBJECT
